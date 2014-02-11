24099737bdf6ffc4e0923451a0ae16c9ad0f6a9e
March Liu <march.liu@gmail.com>
2014-02-11 22:05:59 +0800

    add panic in in extract field function

--- extractor.go
+++ extractor.go
@@ -40,7 +40,7 @@ func SelectExtractor(val reflect.Value) func(reflect.Value)interface{} {
 }
 
 func ExtractField(val reflect.Value, field reflect.StructField) interface{}{
-	var itf interface{} = Extract(val)
+	itf interface{} := Extract(val)
 	if field.Tag.Get("jsonto") != "" {
 		itf, err := json.Marshal(itf)
 		if err != nil {
