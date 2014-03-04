ed8f8f8ecce66c904b0f4994954d869e34d06494
jnriver <jnriver1986@gmail.com>
2014-03-04 15:21:48 +0800

    Fix limit offset bug

--- exp/select.go
+++ exp/select.go
@@ -1,7 +1,7 @@
 // TODO：还没有处理字符串转义，先不要实用，会被注入
 // TODO: Stringer 接口应该改为exp接口，exp.evel接受env参数
 // NOTE:Table 的 name 应该是模型的类型名而非表名，表名只在注册模型的时候显式出现
-// 
+//
 package exp
 
 import (
@@ -10,48 +10,49 @@ import (
 )
 
 // desc 应该能作用到确定的排序字段
-type Sel struct{
+type Sel struct {
 	selects []Exp
-	from *Table
-	join []Exp
-	where Exp
+	from    *Table
+	join    []Exp
+	where   Exp
 	groupby []Exp
-	having Exp
+	having  Exp
 	orderby []Exp
-	limit *int
-	offset *int
+	limit   *int
+	offset  *int
 }
-func SelectThem(fields... string) *Sel {
+
+func SelectThem(fields ...string) *Sel {
 	var fs = make([]Exp, 0)
 	for _, fname := range fields {
 		fs = append(fs, &Field{nil, fname, ""})
 	}
-	return &Sel{selects:fs,
-		from: nil,
-		join:nil,
-		where:nil,
-		groupby:nil,
-		having:nil,
-		orderby:nil,
-		limit:nil,
+	return &Sel{selects: fs,
+		from:    nil,
+		join:    nil,
+		where:   nil,
+		groupby: nil,
+		having:  nil,
+		orderby: nil,
+		limit:   nil,
 	}
 }
-func Select(fields... Exp) *Sel {
-	return &Sel{selects:fields,
-		from: nil,
-		join:nil,
-		where:nil,
-		groupby:nil,
-		having:nil,
-		orderby:nil,
-		limit:nil,
+func Select(fields ...Exp) *Sel {
+	return &Sel{selects: fields,
+		from:    nil,
+		join:    nil,
+		where:   nil,
+		groupby: nil,
+		having:  nil,
+		orderby: nil,
+		limit:   nil,
 	}
 }
 
-func (sel *Sel)From(t *Table) *Sel{
+func (sel *Sel) From(t *Table) *Sel {
 	sel.from = t
 	for _, f := range sel.selects {
-		if field, ok := f.(*Field);ok{
+		if field, ok := f.(*Field); ok {
 			if field.Table == nil {
 				field.Table = t
 			}
@@ -59,50 +60,51 @@ func (sel *Sel)From(t *Table) *Sel{
 	}
 	return sel
 }
-func (sel *Sel)Join(t *Table, on Exp) *Sel{
+func (sel *Sel) Join(t *Table, on Exp) *Sel {
 	if sel.join == nil {
 		sel.join = make([]Exp, 0)
 	}
 	sel.join = append(sel.join, joinExp(t).onExp(on))
 	return sel
 }
-func (sel *Sel)Where(exp Exp) *Sel{
+func (sel *Sel) Where(exp Exp) *Sel {
 	sel.where = whereExp(exp)
 	return sel
 }
-func (sel *Sel)GroupBy(fields... Exp) *Sel{
+func (sel *Sel) GroupBy(fields ...Exp) *Sel {
 	if sel.groupby == nil {
 		sel.groupby = make([]Exp, 0)
 	}
-	for _, f := range fields{
+	for _, f := range fields {
 		sel.groupby = append(sel.groupby, f)
 	}
 	return sel
 }
-func (sel *Sel)Having(exp Exp) *Sel{
+func (sel *Sel) Having(exp Exp) *Sel {
 	sel.having = exp
 	return sel
 }
-func (sel *Sel)OrderBy(fields... Exp) *Sel{
+func (sel *Sel) OrderBy(fields ...Exp) *Sel {
 	if sel.orderby == nil {
-		sel.orderby=fields
+		sel.orderby = fields
 	} else {
-		for _, f := range fields{
+		for _, f := range fields {
 			sel.orderby = append(sel.orderby, f)
 		}
 	}
 	return sel
 }
+
 // 本来我想把 limit 和 offset 设定成 Exp ，但是想来想去写了这么多SQL从来没有这样的用法咯……
-func (sel *Sel)Limit(limit int) *Sel{
+func (sel *Sel) Limit(limit int) *Sel {
 	sel.limit = &limit
 	return sel
 }
-func (sel *Sel)Offset(offset int) *Sel{
+func (sel *Sel) Offset(offset int) *Sel {
 	sel.offset = &offset
 	return sel
 }
-func (sel Sel)Eval(env Env)string{
+func (sel Sel) Eval(env Env) string {
 	var scope = env.Scope()
 	env.SetScope(sel)
 	defer env.SetScope(scope)
@@ -116,11 +118,11 @@ func (sel Sel)Eval(env Env)string{
 		command += strings.Join(fields, ", ")
 	}
 	if sel.from != nil {
-		command += (" FROM "+sel.from.Eval(env))
+		command += (" FROM " + sel.from.Eval(env))
 	}
 	if sel.join != nil {
 		for _, j := range sel.join {
-			command += (" "+j.Eval(env))
+			command += (" " + j.Eval(env))
 		}
 	}
 	if sel.where != nil {
@@ -131,23 +133,23 @@ func (sel Sel)Eval(env Env)string{
 		for _, g := range sel.groupby {
 			groups = append(groups, g.Eval(env))
 		}
-		command += (" GROUP BY "+strings.Join(groups, ", "))
+		command += (" GROUP BY " + strings.Join(groups, ", "))
 	}
 	if sel.having != nil {
-		command += " HAVING "+sel.having.Eval(env)
+		command += " HAVING " + sel.having.Eval(env)
 	}
 	if sel.orderby != nil {
 		var orderby = make([]string, 0)
 		for _, o := range sel.groupby {
 			orderby = append(orderby, o.Eval(env))
 		}
-		command += (" ORDER BY "+strings.Join(orderby, ", "))
+		command += (" ORDER BY " + strings.Join(orderby, ", "))
 	}
 	if sel.limit != nil {
-		command += fmt.Sprintf(" LIMIT %d", sel.limit)
+		command += fmt.Sprintf(" LIMIT %d", *sel.limit)
 	}
 	if sel.offset != nil {
-		command += fmt.Sprintf(" OFFSET %d", sel.offset)
+		command += fmt.Sprintf(" OFFSET %d", *sel.offset)
 	}
 	return command
 }
